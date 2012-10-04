c6b183101b0e05dc08440b6fbf761c30068f7b40
marek <marek@localhost>
2012-10-04 16:18:32 +0200

    fixed hiding list views - wrap_content -> fill_parent

---
+++ src/pl/polidea/navigator/menu/MenuImport.java
@@ -0,0 +1,40 @@
+package pl.polidea.navigator.menu;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import pl.polidea.navigator.JsonMenuReader;
+
+/**
+ * Menu importing from another file.
+ */
+public class MenuImport extends AbstractNavigationMenu {
+    private static final long serialVersionUID = 1L;
+
+    public MenuImport(final JsonMenuReader reader, final JSONObject jsonMenu, final AbstractNavigationMenu parent)
+            throws JSONException {
+        super(reader, jsonMenu, BasicMenuTypes.MENU_IMPORT, parent);
+        link = reader.readLink(jsonMenu, directory, parent);
+    }
+
+    public AbstractNavigationMenu link;
+
+    @Override
+    public boolean isDisabled() {
+        return link == null;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuImport [link=" + link + ", " + super.toString() + "]";
+    }
+
+    @Override
+    public void updateTransientAttributes(final MenuContext menuContext, final AbstractNavigationMenu parent) {
+        super.updateTransientAttributes(menuContext, parent);
+        if (link != null) {
+            link.updateTransientAttributes(menuContext, this);
+        }
+    }
+
+}
---
+++ src/pl/polidea/navigator/ui/OnMenuUpListener.java
@@ -0,0 +1,10 @@
+package pl.polidea.navigator.ui;
+
+/**
+ * Listener responding to menu up event.
+ */
+public interface OnMenuUpListener {
+
+    void onMenuUp(String name);
+
+}
