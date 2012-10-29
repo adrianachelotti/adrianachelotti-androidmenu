02d7f3920afb9d1b678a2d61511de6d7fa545450
Marek Multarzynski <Marek.Multarzynski@polidea.pl>
2012-10-29 15:18:18 +0100

    created test project

---
+++ test/robolectric/src/test/java/pl/polidea/navigator/JsonParser.java
@@ -0,0 +1,26 @@
+package pl.polidea.navigator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+/**
+ *
+ */
+
+/**
+ * @author Marek Multarzynski
+ * 
+ */
+@RunWith(RobolectricTestRunner.class)
+public class JsonParser {
+
+    @Test
+    public void test() {
+        fail();
+    }
+
+}
